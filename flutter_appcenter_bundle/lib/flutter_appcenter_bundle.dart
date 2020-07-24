1876c6fd837637cf4f331734761602d6b9a768ec
hanabi1224 <harlowmoo@gmail.com>
2020-07-24 10:09:11 +0800

    fix pub score

--- flutter_appcenter_bundle/lib/flutter_appcenter_bundle.dart
+++ flutter_appcenter_bundle/lib/flutter_appcenter_bundle.dart
@@ -8,7 +8,9 @@ import 'package:flutter/services.dart';
 final _methodChannelName = "com.github.hanabi1224.flutter_appcenter_bundle";
 final _methodChannel = MethodChannel(_methodChannelName);
 
+/// Static class that provides AppCenter APIs
 class AppCenter {
+  /// Start appcenter functionalities
   static Future startAsync({
     @required String appSecretAndroid,
     @required String appSecretIOS,
@@ -47,6 +49,7 @@ class AppCenter {
     });
   }
 
+  /// Track events
   static Future trackEventAsync(String name,
       [Map<String, String> properties]) async {
     await _methodChannel.invokeMethod('trackEvent', <String, dynamic>{
@@ -55,42 +58,54 @@ class AppCenter {
     });
   }
 
+  /// Check whether analytics is enalbed
   static Future<bool> isAnalyticsEnabledAsync() async {
     return await _methodChannel.invokeMethod('isAnalyticsEnabled');
   }
 
+  /// Get appcenter installation id
   static Future<String> getInstallIdAsync() async {
-    return await _methodChannel.invokeMethod('getInstallId').then((r) => r as String);
+    return await _methodChannel
+        .invokeMethod('getInstallId')
+        .then((r) => r as String);
   }
 
+  /// Enable or disable analytics
   static Future configureAnalyticsAsync({@required enabled}) async {
     await _methodChannel.invokeMethod('configureAnalytics', enabled);
   }
 
+  /// Check whether crashes is enabled
   static Future<bool> isCrashesEnabledAsync() async {
     return await _methodChannel.invokeMethod('isCrashesEnabled');
   }
 
+  /// Enable or disable appcenter crash reports
   static Future configureCrashesAsync({@required enabled}) async {
     await _methodChannel.invokeMethod('configureCrashes', enabled);
   }
 
+  /// Check whether appcenter distribution is enabled
   static Future<bool> isDistributeEnabledAsync() async {
     return await _methodChannel.invokeMethod('isDistributeEnabled');
   }
 
+  /// Enable or disable appcenter distribution
   static Future configureDistributeAsync({@required enabled}) async {
     await _methodChannel.invokeMethod('configureDistribute', enabled);
   }
 
+  /// Enable or disable appcenter distribution for debug build (Android only)
   static Future configureDistributeDebugAsync({@required enabled}) async {
     await _methodChannel.invokeMethod('configureDistributeDebug', enabled);
   }
 
+  /// Disable automatic check for app updates
   static Future _disableAutomaticCheckForUpdateAsync() async {
     await _methodChannel.invokeMethod('disableAutomaticCheckForUpdate');
   }
 
+  /// Manually check for app updates
   static Future checkForUpdateAsync() async {
     await _methodChannel.invokeMethod('checkForUpdate');
   }
